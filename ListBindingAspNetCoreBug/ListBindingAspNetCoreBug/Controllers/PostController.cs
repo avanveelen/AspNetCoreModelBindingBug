d97983b38d87756ef839a2f1a62a9531199d8300
Alexander van Veelen <alexander@vicompany.nl>
2018-12-23 11:34:34 +0100

    Initial commit

---
+++ ListBindingAspNetCoreBug/ListBindingAspNetCoreBug/Controllers/PostController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ListBindingAspNetCoreBug.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListBindingAspNetCoreBug.Controllers
+{
+    public class PostController : Controller
+    {
+        [HttpPost("post")]
+        public IActionResult Index(List<Content> content)
+        {
+
+            return Content("Reached Action");
+        }
+
+        [HttpPost("post2")]
+        public IActionResult Index2(Content content)
+        {
+
+            return Content("Reached Action 2");
+        }
+    }
+}
---
+++ ListBindingAspNetCoreBug/ListBindingAspNetCoreBug/Models/Content.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace ListBindingAspNetCoreBug.Models
+{
+    public class Content
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
